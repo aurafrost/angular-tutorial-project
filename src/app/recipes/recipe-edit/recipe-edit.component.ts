e049d6b3e39dc0c34e7e51330caee4418ceec8ea
jimmytran <jt963369@gmail.com>
2019-03-07 12:58:38 -0500

    recipe edit now working mostly

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
-import { FormGroup, FormControl, FormArray } from '@angular/forms';
+import { FormGroup, FormControl, FormArray, Validators } from '@angular/forms';
 import { RecipeService } from '../recipe.service';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -39,8 +40,8 @@ export class RecipeEditComponent implements OnInit {
         for(let ingredient of recipe.ingredients){
           recipeIngredients.push(
             new FormGroup({
-              'name':new FormControl(ingredient.name),
-              'amount':new FormControl(ingredient.amount)
+              'name':new FormControl(ingredient.name, Validators.required),
+              'amount':new FormControl(ingredient.amount, [Validators.required,Validators.min(1),Validators.pattern(/^[1-9]+[0-9]*$/)])
             })
           );
         }
@@ -48,10 +49,34 @@ export class RecipeEditComponent implements OnInit {
     }
 
     this.recipeForm = new FormGroup({
-      'name': new FormControl(recipeName),
-      'imagePath': new FormControl(recipeImagePath),
-      'description': new FormControl(recipeDescription),
+      'name': new FormControl(recipeName, Validators.required),
+      'imagePath': new FormControl(recipeImagePath, Validators.required),
+      'description': new FormControl(recipeDescription, Validators.required),
       'ingredients': recipeIngredients,
     });
+  }
+
+  addIngredient(){
+    (<FormArray>this.recipeForm.get('ingredients')).push(
+      new FormGroup({
+        'name':new FormControl(null,Validators.required),
+        "amount":new FormControl(null,[Validators.required,Validators.min(1),Validators.pattern(/^[1-9]+[0-9]*$/)]),
+      })
+    );
+  }
+
+  onSubmit(){
+    // const recipe=new Recipe(this.recipeForm.value['name'],this.recipeForm.value['description'],this.recipeForm.value['imagePath'],this.recipeForm.value['ingredients']);
+    if(this.editMode){
+      this.recipeService.updateRecipe(this.id,this.recipeForm.value);
+    }
+    else{
+      this.recipeService.addRecipe(this.recipeForm.value);
+    }
+    this.resetForm();
+  }
+
+  resetForm(){
+    this.recipeForm.reset();
   }
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -2,17 +2,19 @@ import { Injectable, EventEmitter } from '@angular/core';
 import { Recipe } from './recipe.model';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list/shopping-list.service';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RecipeService {
+
+  recipesChanged=new Subject<Recipe[]>();
   private recipes: Recipe[] = [
     new Recipe("Hot Dog", "As shown.", "/assets/images/hotdog.jpg",[new Ingredient("Hot dog",1),new Ingredient("Bun",1)]),
     new Recipe("Spaghetti and Meatballs", "Meatballs not optional.", "https://img.taste.com.au/hyE5dSN3/taste/2016/11/spaghetti-with-meatballs-and-spicy-tomato-sauce-102298-1.jpeg",[new Ingredient("Pasta",2),new Ingredient("Meatballs",2),new Ingredient("Grated Cheese",1)]),    
     new Recipe("Sandwich", "Typical sandwich.", "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/delish-thanksgiving-leftover-sandwich-horizontal-1542326155.jpg?crop=0.846xw:0.634xh;0.153xw,0.0998xh&resize=1200:*",[new Ingredient("Bread",2),new Ingredient("Meat",2),new Ingredient("Lettuce",1),new Ingredient("Tomato",2)]),  
   ];
-  recipeSelected=new EventEmitter<Recipe>();
 
   constructor(private service:ShoppingListService) { }
 
@@ -26,5 +28,15 @@ export class RecipeService {
 
   addIngredients(ingredients:Ingredient[]){
     this.service.addIngredients(ingredients);
+  }
+
+  addRecipe(recipe:Recipe){
+    this.recipes.push(recipe);
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  updateRecipe(index:number,newRecipe:Recipe){
+    this.recipes[index]=newRecipe;
+    this.recipesChanged.next(this.recipes.slice());
   }
 }
